febf7ccabe7332155783c5d9b76a48188a643138
Luke Gra <lukgralak@gmail.com>
2021-06-01 14:11:36 +0200

    tests

--- test.py
+++ test.py
@@ -1,12 +1,32 @@
 from app import app
+from io import StringIO, BytesIO
 import unittest
 
-class FlaskTestCase(unittest.TestCase):
+class TestCase(unittest.TestCase):
 
 	# Check if response is 200
-	def test_index(self):
+	def test_index_load(self):
 		tester = app.test_client(self)
-		response = tester.get('/', content_type='html/text')
+		response = tester.get('/', content_type = 'html/text')
+		self.assertEqual(response.status_code, 200)
+
+	def test_index_content(self):
+		tester = app.test_client(self)
+		response = tester.get('/', content_type = 'html/text')
+		self.assertTrue(b'Detect!' in response.data)
+
+	def test_upload(self):
+		tester = app.test_client(self)
+
+		with open('static/images/test.png', 'rb') as img:
+			imgBytesIO = BytesIO(img.read())
+
+		response = tester.post(
+			'/',
+			content_type = 'multipart/form-data',
+			data = {'file': (imgBytesIO, 'test.png')},
+			follow_redirects = True
+		)
 		self.assertEqual(response.status_code, 200)
 
 if __name__=='__main__':
